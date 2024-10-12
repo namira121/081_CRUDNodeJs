cda737cef18e15a9fafb73fa07d1dd0bcea49849
namira121 <127211008+namira121@users.noreply.github.com>
2024-10-12 12:17:57 +0700

    node,express install and setting port

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 8000;
+
+app.use(bodyParser.json());
+app.listen(PORT, () => console.log(`Server berjalan di port : http://localhost${PORT}`));
